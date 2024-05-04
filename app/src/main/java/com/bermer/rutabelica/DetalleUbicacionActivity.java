2b7e1075657a0fd483a2f414b9d5364504ffdf99
Carlos Bernal <78837548+bernal9913@users.noreply.github.com>
2024-05-03 23:25:24 -0700

    primeras fases de ruta

--- app/src/main/java/com/bermer/rutabelica/DetalleUbicacionActivity.java
+++ app/src/main/java/com/bermer/rutabelica/DetalleUbicacionActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -18,13 +21,18 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class DetalleUbicacionActivity extends AppCompatActivity {
+    private ArrayList<LatLng> ubicaciones = new ArrayList<>();
     private TextView textTitulo;
     private TextView textDescripcion;
     private TextView textCiudadEstado;
     private TextView textCategoria;
     private TextView textAptoDiscapacitados;
     private TextView textPetFriendly;
+    private Button btnAgregarRuta;
+    private Button btnReportar;
     private double latatitu;
     private double longitu;
     //private GoogleMap mMap;
@@ -43,6 +51,9 @@ public class DetalleUbicacionActivity extends AppCompatActivity {
         textCategoria = findViewById(R.id.textCategoria);
         textAptoDiscapacitados = findViewById(R.id.textAptoDiscapacitados);
         textPetFriendly = findViewById(R.id.textPetFriendly);
+        btnAgregarRuta = findViewById(R.id.btnAgregar);
+        btnReportar = findViewById(R.id.btnReportar);
+
 
         titulo = getIntent().getStringExtra("titulo");
         String descripcion = getIntent().getStringExtra("descripcion");
@@ -54,6 +65,7 @@ public class DetalleUbicacionActivity extends AppCompatActivity {
         String latitud = getIntent().getStringExtra("latitud");
         latatitu = Double.parseDouble(latitud);
         longitu = Double.parseDouble(longitud);
+        ubicaciones = getIntent().getParcelableArrayListExtra("ubicaciones");
 
         Log.d("Mapa", "Latitud: " + latatitu + " Longitud: " + longitu);
 
@@ -77,6 +89,43 @@ public class DetalleUbicacionActivity extends AppCompatActivity {
         textPetFriendly.setText("Pet friendly: " + (petFriendly ? "Sí" : "No"));
         FrameLayout mapContainer = findViewById(R.id.map_container);
         mapContainer.addView(mapView);
+
+        btnAgregarRuta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ListaUbicaciones.ubicaciones.add(new LatLng(latatitu, longitu));
+                // crear el punto
+                LatLng ubisend = new LatLng(latatitu, longitu);
+                // agregar a la lista de ubicaciones
+                ubicaciones.add(ubisend);
+                // enviar la lista de ubicaciones
+                Intent intent = new Intent();
+                //intent.putParcelableArrayListExtra("ubicaciones", ubicaciones);
+                intent.putExtra("longitud", longitud);
+                intent.putExtra("latitud", latitud);
+                intent.putExtra("titulo", titulo);
+                setResult(RESULT_OK, intent);
+                Toast.makeText(DetalleUbicacionActivity.this, titulo.toString() + " ha sido agregado a la ruta!", Toast.LENGTH_SHORT).show();
+                finish();
+
+            }
+        });
+        btnReportar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DetalleUbicacionActivity.this, ReportActivity.class);
+                intent.putExtra("titulo", titulo);
+                intent.putExtra("descripcion", descripcion);
+                intent.putExtra("ciudad", ciudadEstado);
+                intent.putExtra("categoria", categoria);
+                intent.putExtra("aptoDiscapacitados", aptoDiscapacitados);
+                intent.putExtra("petFriendly", petFriendly);
+                intent.putExtra("longitud", longitud);
+                intent.putExtra("latitud", latitud);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         /*
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
