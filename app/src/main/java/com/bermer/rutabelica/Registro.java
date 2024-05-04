14452e5bf19beaad530883482b337013d905b0fc
Carlos Bernal <78837548+bernal9913@users.noreply.github.com>
2024-05-04 14:26:27 -0700

    limpieza de codigo

--- app/src/main/java/com/bermer/rutabelica/Registro.java
+++ app/src/main/java/com/bermer/rutabelica/Registro.java
@@ -1,21 +1,6 @@
 package com.bermer.rutabelica;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.AuthResult;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.TextUtils;
@@ -28,10 +13,21 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 //import
 
 public class Registro extends AppCompatActivity {
@@ -83,7 +79,7 @@ public class Registro extends AppCompatActivity {
 
     }
     private void startEverything(){
-        System.out.println("start registro");
+        Log.d("Start registro", "startEverything: ");
         editTextFullName = findViewById(R.id.editTextFullName);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -105,51 +101,13 @@ public class Registro extends AppCompatActivity {
         String dob = editTextDateOfBirth.getText().toString().trim();
         String c = spinnerCountry.getSelectedItem().toString();
 
-        /*
-        if (TextUtils.isEmpty(fn) || TextUtils.isEmpty(e) || TextUtils.isEmpty(p) ||
-                TextUtils.isEmpty(rp) || TextUtils.isEmpty(pn) || TextUtils.isEmpty(dob) ||
-                TextUtils.isEmpty(c) || !terms.isChecked()) {
-            System.out.println("campos llenos");
-            Toast.makeText(Registro.this, "Todos los campos deben estar llenos", Toast.LENGTH_SHORT).show();
-        } else {
-            if (!p.equals(rp)) {
-                System.out.println("contraseña no coincidente");
-                Toast.makeText(Registro.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
-            } else if (!terms.isChecked()) {
-                Toast.makeText(Registro.this, "Acepta los términos y condiciones", Toast.LENGTH_SHORT).show();
-            } else {
-                System.out.println("creacion del user");
-                Map<String, Object> user = new HashMap<>();
-                user.put("fullname", fn);
-                user.put("email", e);
-                user.put("password", p);
-                user.put("phone", pn);
-                user.put("dob", dob);
-
-                db.collection("users").document(e)
-                        .set(user)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if(task.isSuccessful()){
-                                    System.out.println("todo salio bien ");
-                                    Toast.makeText(Registro.this, "El registro fue exitoso?", Toast.LENGTH_SHORT);
-                                }else {
-                                    System.out.println("todo salio mal?");
-                                    Toast.makeText(Registro.this, "Error: " + task.getException().getMessage().toString(), Toast.LENGTH_SHORT);
-                                }
-                            }
-                        });
-                // TODO: (24.10.3) [WriteStream]: (af06997) Stream closed with status: Status{code=PERMISSION_DENIED, description=Missing or insufficient permissions., cause=null}.
-                // 2024-02-28 22:14:59.792  6949-7011  Firestore               com.bermer.rutabelica                W  (24.10.3) [Firestore]: Write failed at users/meraz@gmail.com: Status{code=PERMISSION_DENIED, description=Missing or insufficient permissions., cause=null}
-                // 2024-02-28 22:14:59.793  6949-6949  System.out              com.bermer.rutabelica                I  todo salio mal?
-            }
-        }
-        */
+        if(!terms.isChecked()){{
+            Toast.makeText(Registro.this, "Debe aceptar los términos y condiciones", Toast.LENGTH_SHORT).show();
+            return;
+        }}
 
         if (TextUtils.isEmpty(fn) || TextUtils.isEmpty(e) || TextUtils.isEmpty(p) ||
-                TextUtils.isEmpty(rp) || TextUtils.isEmpty(pn) || TextUtils.isEmpty(dob) ||
-                !terms.isChecked()) {
+                TextUtils.isEmpty(rp) || TextUtils.isEmpty(pn) || TextUtils.isEmpty(dob) || TextUtils.isEmpty(c)) {
             Toast.makeText(Registro.this, "Todos los campos deben estar llenos y aceptar los términos y condiciones", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -180,6 +138,7 @@ public class Registro extends AppCompatActivity {
                                         @Override
                                         public void onSuccess(Void aVoid) {
                                             Log.d("Registro_debug", "Documento subido correctamente a Firestore con ID: " + e);
+                                            finish();
                                             //Toast.makeText(Registro.this, "Información del usuario subida exitosamente", Toast.LENGTH_SHORT).show();
                                         }
                                     })
@@ -190,21 +149,6 @@ public class Registro extends AppCompatActivity {
                                             //Toast.makeText(Registro.this, "Error al subir información del usuario", Toast.LENGTH_SHORT).show();
                                         }
                                     });
-                            /*usersRef.add(users)
-                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                        @Override
-                                        public void onSuccess(DocumentReference documentReference) {
-                                            Log.d("Registro_debug", "Documento subido correctamente a Firestore con ID: " + documentReference.getId());
-                                            //Toast.makeText(Registro.this, "Información del lugar subida exitosamente", Toast.LENGTH_SHORT).show();
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Log.e("Registro_debug", "Error al subir datos a Firestore: " + e.getMessage());
-                                            //Toast.makeText(AddLocation.this, "Error al subir información del lugar: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                        }
-                                    });*/
                             finish();
                         } else {
                             // Error durante el registro
